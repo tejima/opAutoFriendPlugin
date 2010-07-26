db8c686c21fb04863fd58137e5381d3618aa4a8f
手嶋守 <tejima@gmail.com>
2010-07-26 07:41:20 -0700

    ログイン停止中のメンバーとはフレンドにならない設定に。

--- lib/task/opAutoFriendTask.class.php
+++ lib/task/opAutoFriendTask.class.php
@@ -43,7 +43,7 @@ EOF;
     //最初に全フレンドリンクを削除
     Doctrine_Query::create()->delete('MemberRelationship s')->execute();
     $conn = $databaseManager->getDatabase(array_shift($databaseManager->getNames()))->getConnection();
-    $stmt = $conn->prepare('insert into member_relationship (member_id_to,member_id_from,is_friend,is_friend_pre) SELECT m1.id as member_id_to ,m2.id as member_id_from ,1 as is_friend , 0 as is_friend_pre FROM member as m1,member as m2 WHERE m1.id != m2.id');
+    $stmt = $conn->prepare('insert into member_relationship (member_id_to,member_id_from,is_friend,is_friend_pre) SELECT m1.id as member_id_to ,m2.id as member_id_from ,1 as is_friend , 0 as is_friend_pre FROM member as m1,member as m2 WHERE m1.id != m2.id AND m1.is_login_rejected = 0 AND m2.is_login_rejected = 0');
     $stmt->execute();
   }
   private function autoFriendWithCommunityId($target_id = null){
@@ -85,7 +85,7 @@ EOF;
     Doctrine_Query::create()->delete()->from('MemberRelationship')
       ->where('member_id_to = ?',$target_id)->orWhere('member_id_from = ?',$target_id)->execute();
 
-    $stmt = $conn->prepare('insert into member_relationship (member_id_to,member_id_from,is_friend,is_friend_pre) select id as member_id_to , ? as member_id_from, 1 as is_friend, 0 as is_friend_pre from member where id != ? UNION select ? as member_id_to , id as member_id_from , 1 as is_friend, 0 as is_friend_pre from member where id !=?;');
+    $stmt = $conn->prepare('insert into member_relationship (member_id_to,member_id_from,is_friend,is_friend_pre) select id as member_id_to , ? as member_id_from, 1 as is_friend, 0 as is_friend_pre from member where id != ? AND is_login_rejected = 0 UNION select ? as member_id_to , id as member_id_from , 1 as is_friend, 0 as is_friend_pre from member where id !=? AND is_login_rejected = 0 ;');
     $stmt->execute(array($target_id,$target_id,$target_id,$target_id));
   }
 }
